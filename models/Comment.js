c9f3f880dd565da8464f8948691913af5dc81e50
Mihaela <talmazanmihaela@gmail.com>
2025-01-13 10:27:10 +0100

    Added User, Post, Like, Comment models

---
+++ models/Comment.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  post_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  comment_text: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
---
+++ models/Post.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  images: [{ type: String, required: true }],
+  caption: { type: String, default: "" },
+  likes_count: { type: Number, default: 0 },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Like",
+      required: true,
+    },
+  ],
+  comments_count: { type: Number, default: 0 },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      required: true,
+    },
+  ],
+  created_at: { type: Date, default: Date.now },
+});
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
