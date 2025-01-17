45214b48ee762499eca5ccbe0726e3d259cf3c19
Mihaela <talmazanmihaela@gmail.com>
2025-01-17 23:45:51 +0100

    Add get bost by id

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: "50mb" })); // Increase the limit as needed
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 connectDB();
