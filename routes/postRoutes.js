e0495488572900c9eeca4322ccbed483d759b1a5
Mihaela <talmazanmihaela@gmail.com>
2025-01-21 17:43:30 +0100

    Add post update endpoint

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { createPost, getPostById, getUserPosts } from "../controllers/post.js";
+import {
+  createPost,
+  deletePost,
+  getPostById,
+  getUserPosts,
+  updatePost,
+} from "../controllers/post.js";
 import upload from "../middlewares/multer.js";
 
 const router = Router();
@@ -8,5 +14,7 @@ const router = Router();
 router.post("/", authMiddleware, upload.single("images"), createPost);
 router.get("/all", authMiddleware, getUserPosts);
 router.get("/single/:postId", authMiddleware, getPostById);
+router.delete("/:postId", authMiddleware, deletePost);
+router.put("/:postId", authMiddleware, updatePost);
 
 export default router;
